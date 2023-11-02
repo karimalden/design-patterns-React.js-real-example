1373d16b2b193fbcc39600372411ae151278a4ea
karimalden <karimaldenlibrary@gmail.com>
2023-11-02 17:27:48 +0300

    new Cor Pattern

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import ExampleSingleton from './Creational/Singleton/ExampleSingleton'
 import ConcretePrototype from './Creational/Prototype';
 import ExamplePrototype from './Creational/Prototype/ExamplePrototype';
 import ExampleFactory from './Creational/Factory Method/ExampleFactory';
+import ExampleCor from './Behavioral/Chain of Responsibility/ExampleCor';
 
 function App() {
 
@@ -20,6 +21,11 @@ function App() {
 
   //4. Builder 
   // ExampleSingleton()
+
+  // Behavioral 
+
+  //1.Chain Of Responsibilty
+  return ExampleCor()
   return (
     <div>
        React.js Design Pattern With Real Example  
